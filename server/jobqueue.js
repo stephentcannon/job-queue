4697b7a29bdde34ccc6377671f26a26b51f1ed11
system_user <stephentcannon@gmail.com>
2014-07-09 18:48:28 -0400

    added remove job

--- server/jobqueue.js
+++ server/jobqueue.js
@@ -1,9 +1,16 @@
 Jobqueue = new Meteor.Collection('jobqueue');
 
 Jobqueue.addJob = function(job){
+  console.log('Jobqueue.addJob fired with job');
+  console.log(job);
   job.status = 'unprocessed';
   Jobqueue.insert(job);
+};
+
+Jobqueue.removeJob = function(job_id){
+  Jobqueue.remove({_id: job_id});
 }
+
 
 
 
